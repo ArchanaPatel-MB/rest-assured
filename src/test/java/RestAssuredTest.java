1a9b1be64452c5f1c7dfe7a0942b3222e2c235be
Archana Patel <archana.patel.16@mountblue.tech>
2021-08-20 16:13:00 +0530

    test case added for the id check and the status check

--- src/test/java/RestAssuredTest.java
+++ src/test/java/RestAssuredTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
         import io.restassured.response.Response;
         import io.restassured.specification.RequestSpecification;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class RestAssuredTest {
 
     @Test
@@ -18,5 +20,71 @@ public class RestAssuredTest {
         System.out.println("Response Body is =>  " + responseBody);
 
     }
+
+    @Test
+    public void todoistGetRequest(){
+        String token="c0d2f28135872130984acf1ccf39461203f233db";
+        RestAssured.baseURI = "https://api.todoist.com/rest/v1/tasks";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.header( "Authorization", "Bearer "+token).header("Content-Type","application/json").get("https://api.todoist.com/rest/v1/tasks");
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body is =>  " + responseBody);
+    }
+
+    @Test
+  public void StatusCheck() {
+                String token="c0d2f28135872130984acf1ccf39461203f233db";
+        RestAssured
+                .given()
+                .header( "Authorization", "Bearer "+token).header("Content-Type","application/json").get("https://api.todoist.com/rest/v1/projects/2271791984")
+                        .then()
+                        .statusCode(200)
+                        .log()
+                        .all();
+    }
+
+    @Test
+    public void taskStatusCheck(){
+
+        String token="c0d2f28135872130984acf1ccf39461203f233db";
+        RestAssured
+                .given()
+                .header( "Authorization", "Bearer "+token).header("Content-Type","application/json").get("https://api.todoist.com/rest/v1/tasks")
+                .then()
+                .statusCode(200)
+                .log()
+                .all();
+
+    }
+    @Test
+     public void idCheck(){
+        String token="c0d2f28135872130984acf1ccf39461203f233db";
+        RestAssured
+                .given()
+                .header( "Authorization", "Bearer "+token).header("Content-Type","application/json").get("https://api.todoist.com/rest/v1/tasks/5081923746")
+                .then()
+                .assertThat()
+                .body("id", equalTo(5081923746L))
+                .log()
+                .all();
+
+    }
+
+
+    @Test
+    public void contentCheck(){
+        String token="c0d2f28135872130984acf1ccf39461203f233db";
+        RestAssured
+                .given()
+                .header( "Authorization", "Bearer "+token).header("Content-Type","application/json").get("https://api.todoist.com/rest/v1/tasks/5081923746")
+                .then()
+                .assertThat()
+                .body("content", equalTo("Click this task to see more details")
+                .log()
+                .all();
+
+    }
+
+
 
 }
